f6f479ebd7d14fb3bbe6af06293a10bc1286c24f
truckstoptrixy <truckstoptrixy@gmail.com>
2018-01-23 14:09:02 -0800

    paperclip added and migrates

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
-class Post < ApplicationRecord
+class Post < ActiveRecord::Base[5.1.4]
+  validates :image, presence: true
+
+  has_attached_file :image, styles: { :medium => "640x" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
