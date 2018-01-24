223ca5545dfe0acf40bcb8c9d08966f2196645f8
truckstoptrixy <truckstoptrixy@gmail.com>
2018-01-24 14:07:11 -0800

    working backgrounds and placeholders

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_model!
+  before_action :set_post, only: [:show, :edit, :update, :destroy, :like]
   before_action :owned_post, only: [:edit, :update, :destroy]
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -27,6 +27,7 @@ class PostsController < ApplicationController
   end
 
   def edit
+
   end
 
   def update
