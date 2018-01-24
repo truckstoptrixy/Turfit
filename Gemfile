6e8ccfdba99c897668b40da9132daef53ea76993
truckstoptrixy <truckstoptrixy@gmail.com>
2018-01-23 16:16:08 -0800

    created actions for images

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 gem 'haml'
 gem "paperclip", "~> 5.0.0"
+gem 'simple_form'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
-class Post < ActiveRecord::Base[5.1.4]
+class Post < ActiveRecord::Base
+
   validates :image, presence: true
 
   has_attached_file :image, styles: { :medium => "640x" }
+
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
 end
